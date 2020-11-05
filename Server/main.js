28e98ac902d3d6a3f4ed5aef8fbf35b847c5d05f
EmmanuelGutierrez3877 <71938979+EmmanuelGutierrez3877@users.noreply.github.com>
2020-11-04 21:50:40 -0600

    port config

--- Server/main.js
+++ Server/main.js
@@ -4,7 +4,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 var messages = [];
-
+var port = process.env.PORT||5000;
 
 app.use(express.static('Public'));
 
@@ -26,7 +26,7 @@ io.on('connection',function(socket){
     });
 });
 
-server.listen(process.env.PORT||5000,function(){
+server.listen(port,function(){
     console.log("server corriendo");
 });
 
